b61492b86146a29dac36f39fb4f23caf39c9c861
David Crowder <david.crowder@cerner.com>
2017-01-13 08:03:18 -0600

    Updated cookbook version to 1.1.0 after release

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'OpsInfraTeam@cerner.com'
 license          'Apache 2.0'
 description      'Installs/Configures ops_tcpdump_handler'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.1.0'
 
 supports 'ubuntu'
 supports 'centos'
